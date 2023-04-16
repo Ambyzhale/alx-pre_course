5eb3b4d3f204e6bd3c17dea36e1e757428d45072
Ambyzhale <ambroseoyar@gmail.com>
2023-04-16 22:55:34 +0300

    add.py

---
+++ add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def add(a , b):
+
+    '''my addition function'''
+
+a = input("enter the value of a")
+b = input("enter the value of b") 
+results = print(int(a)+int(b))
+if __name__ == "__main__":
+    print(results)
